256afb2433bb2e17ea6c6f1bbd05f6047218cbe8
Bo Chen <bochen0909@gmail.com>
2025-01-30 23:52:58 +0000

    Add RandomForestClassificationModel with tests

--- mlservice_tabular/sklearn_model/__init__.py
+++ mlservice_tabular/sklearn_model/__init__.py
@@ -3,5 +3,6 @@ from mlservice_tabular.sklearn_model.ridge import RidgeModel
 from mlservice_tabular.sklearn_model.logistic import LogisticRegressionModel
 from mlservice_tabular.sklearn_model.lasso import LassoModel
 from mlservice_tabular.sklearn_model.random_forest_regression import RandomForestRegressionModel
+from mlservice_tabular.sklearn_model.random_forest_classification import RandomForestClassificationModel
 
-__all__ = ['RidgeModel', 'LogisticRegressionModel', 'LassoModel', 'RandomForestRegressionModel']
+__all__ = ['RidgeModel', 'LogisticRegressionModel', 'LassoModel', 'RandomForestRegressionModel', 'RandomForestClassificationModel']
---
+++ mlservice_tabular/sklearn_model/random_forest_classification.py
@@ -0,0 +1,28 @@
+from typing import Any, Optional
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from mlservice.core.tabml import TabClassification
+from mlservice.core.ml import model_endpoints
+
+@model_endpoints("sklearn/random_forest_classification")
+class RandomForestClassificationModel(TabClassification):
+    def __init__(self, params=None):
+        super().__init__(params)
+        self.model = RandomForestClassifier(**self.hyperparameters)
+
+    def _predict(self, data: pd.DataFrame) -> pd.DataFrame:
+        feature_columns = self._infer_features_columns(data.columns)
+        X = data[feature_columns].values
+        y_pred = self.model.predict(X)
+        y_proba = self.model.predict_proba(X)[:, 1]  # Probability of class 1
+        data[self.prediction_column] = y_pred
+        data[self.predict_proba_column] = y_proba
+        return data
+    
+    def _train(self, train_data: Any, eval_data: Optional[Any] = None):
+        feature_columns = self._infer_features_columns(train_data.columns)
+        self._set_feature_columns(feature_columns)
+        X = train_data[feature_columns].values
+        y = train_data[self.target_column].values
+        self.model.fit(X, y)
+        return self
